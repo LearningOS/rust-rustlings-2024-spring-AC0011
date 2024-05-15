ea0ba704a4ad30156265cd4091f76363094b8b7d
Liu Min <2775690335@qq.com>
2024-05-15 10:53:34 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,21 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32) -> i32{
+    let price= if num<=40
+    {
+        2*num
+    }
+    else
+    {
+        num
+    };
+    return price;
+
+}
 
 // Don't modify this function!
 #[test]
