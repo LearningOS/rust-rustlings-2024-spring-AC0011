7fc0d175aed2e6e8a74cb22bdad2b02520e820cd
Liu Min <2775690335@qq.com>
2024-05-16 11:30:17 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -13,7 +13,7 @@
 struct Package {
     sender_country: String,
     recipient_country: String,
-    weight_in_grams: i32,
+    weight_in_grams: i32
 }
 
 impl Package {
@@ -24,17 +24,24 @@ impl Package {
             Package {
                 sender_country,
                 recipient_country,
-                weight_in_grams,
+                weight_in_grams
             }
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if(self.sender_country!=self.recipient_country)
+        {
+            true
+        }
+        else
+        {
+            false
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams*cents_per_gram
     }
 }
 
