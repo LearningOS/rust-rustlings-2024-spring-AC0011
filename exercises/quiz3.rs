fc8acc733cb381ddb4cfd161df696a1f4615f0c4
Liu Min <2775690335@qq.com>
2024-05-17 15:47:04 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,20 +16,39 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+
+
+pub trait Displaygrade {
+    fn display_grade(&self) -> String;
+}
+
+impl Displaygrade for f32{
+    fn display_grade(&self) -> String{
+        self.to_string()
+    }
+}
+
+impl Displaygrade for &str{
+    fn display_grade(&self) -> String{
+        self.to_string()
+    }
+}
+
+pub struct ReportCard<T:Displaygrade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T:Displaygrade> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.display_grade())
     }
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -52,7 +71,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
